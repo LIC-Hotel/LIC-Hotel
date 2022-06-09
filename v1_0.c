412ae759228103fb53c3daedd1c9249a609008cd
Leonardo Grupioni <52112271+leonardogrupioni@users.noreply.github.com>
2022-06-09 15:45:58 -0300

    Add files via upload

---
+++ v1_0.c
@@ -0,0 +1,40 @@
+//LIC PROJETO HOTEL
+//COM-MA11 INTEGRANTES: LEONARDO GRUPIONI, VICTORIA FERRARESE, E ...
+//VERSÃO 1.0 (INICIO 07/06/2022)
+
+//bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
+
+//constantes definidas
+#define ANDARES 20; //andares do hotel
+#define APTOS 14; //apartamentos por andar
+
+setLocale(LC_ALL, "Portuguese");
+
+int main(){
+
+    //inicialização do menu
+    int op;
+    printf("O que você deseja acessar: \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+
+
+
+
+
+}
+
+void checkin(char matriz[][]){
+
+}
+
