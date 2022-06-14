8f2ecd6327ad7acc06b7fdb21e1755004221d2c0
Leonardo Grupioni <52112271+leonardogrupioni@users.noreply.github.com>
2022-06-13 21:50:38 -0300

    Add files via upload

---
+++ HeaderHotel.h
@@ -0,0 +1,12 @@
+void alocarMatriz(int x[1], int y[1]);
+int testarReserva(int z[1], int w[1]);
+int testarCheckin(int z[1], int w[1]);
+int testarCheckout(int z[1], int w[1]);
+int testarCancelaReserva(int z[1], int w[1]);
+void reservar();
+void checkin();
+void checkout();
+void cancelarReserva();
+void quartosReservado();
+void imprimirMapa();
+
